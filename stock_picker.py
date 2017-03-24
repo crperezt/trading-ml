7bc74bb6276c63f9c3eb772d496d8cdf692e7a0f
crperezt <crperez@gmail.com>
2017-03-23 17:21:07 -0700

    Last update for report and project notebook

--- stock_picker.py
+++ stock_picker.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 from time import time
-from sklearn.metrics import f1_score, classification_report
+from sklearn.metrics import f1_score, classification_report, r2_score
 from sklearn.cross_validation import train_test_split
 from sklearn import naive_bayes
 from sklearn import svm
@@ -12,43 +12,12 @@ from sklearn.metrics import make_scorer
 from sklearn.cross_validation import ShuffleSplit
 from sklearn.model_selection import cross_val_score
 from sklearn.externals import joblib
-from datetime import date
-from config import FORMS_DATA_PATH
-from os.path import join
-#from insider_data import SecCrawler
-
-#import final model -done
-#Generate dataset for one-year
-#Maintain stock symbol
-#Classify dataset
-#List plus10 and minus 10
-
-def create_dataset(from_first=False):
-    #Set date range for last year from today, or this month
-    if from_first:
-        end_date = date.today().replace(day=1)
-        start_date = end_date.replace(year=end_date.year-1)
-    else:
-        end_date = date.today()
-        start_date = end_date.replace(year=end_date.year-1)
-    
-    end_date = end_date.isoformat()[0:10]
-    start_date = start_date.isoformat()[0:10]
-
-    #crawl_new_filings(end_date)
-
-def crawl_new_filings(end_date):
-    lf = open(join(FORMS_DATA_PATH,'last-fetched.txt'),'r+')
-    last_fetched = lf.readline().strip('\n')
-    lf.close()
-    print last_fetched
-
-    s = SecCrawler()
-    s.parse_form4(last_fetched, end_date)
-    lf = open(join(FORMS_DATA_PATH,'last-fetched.txt'),'w')
-    lf.write(end_date)
-
-    return
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.metrics import brier_score_loss
 
 def preprocess_features(X):
 	output = pd.DataFrame(index = X.index)
@@ -58,6 +27,62 @@ def preprocess_features(X):
 		output = output.join(col_data)
 
 	return output
+
+def performance_metric(y_true, y_pred):
+	return f1_score(y_true,y_pred,average='weighted')
+
+def reg_performance_metric(y_true, y_pred):
+    return r2_score(y_true,y_pred)
+
+def train_reg(reg, X_train, y_train, X_test, y_test):
+    reg.fit(X_train, y_train) 
+    scoring_fnc = make_scorer(performance_metric)
+    predict_ret(reg,X_test, y_test)
+
+def train_classifier(clf, X_train, y_train, X_test, y_test):
+    clf.fit(X_train, y_train) 
+    scoring_fnc = make_scorer(performance_metric)
+    predict_labels(clf,X_test, y_test)
+
+    prob_pos_clf = clf.predict_proba(X_test)[:,0]
+
+
+    sorted_prob = zip(clf.predict(X_test),y_test,prob_pos_clf)
+    sorted_prob_df = pd.DataFrame(sorted_prob)
+    print sorted_prob_df.head()
+
+    sorted_prob_df.sort(2,ascending=False,inplace=True)
+    print sorted_prob_df.head(50)
+    print("Brier scores: (the smaller the better)")
+    clf_score = brier_score_loss(y_test, prob_pos_clf, pos_label='above')
+    print("No calibration: %1.3f" % clf_score)
+
+def train_classifierGS(clf, X_train, y_train, params=None):
+    cv_iters = 2
+    cv_sets = ShuffleSplit(X_train.shape[0], n_iter=cv_iters, test_size=0.20, random_state=0)
+    scoring_fnc = make_scorer(performance_metric)
+
+    # Create the grid search object
+    grid = GridSearchCV(clf, params, scoring=scoring_fnc, cv=cv_sets)
+    grid.fit(X_train, y_train)
+    print "best_params_ for the optimal model are: {}.".format(str(grid.best_params_))
+    return grid.best_params_, grid.best_estimator_
+
+def train_classifierCV(clf, X_train, y_train, fit_params=None):
+    ''' Fits a classifier to the training data. '''
+
+    cv_iters = 10
+    cv_sets = ShuffleSplit(X_train.shape[0], n_iter=cv_iters, test_size=0.20, random_state=0)
+    scoring_fnc = make_scorer(performance_metric)  
+
+    start = time()
+    scores_array = cross_val_score(clf, X_train, y=y_train, fit_params=fit_params, scoring=scoring_fnc, cv=cv_sets)
+    end = time()
+    
+    # Print the results
+    print "Trained model in {:.4f} seconds".format(end - start)
+    print "\nCross-Validation scores for " + str(cv_iters) + " iterations:\n"
+    print scores_array
     
 def predict_labels(clf, features, target):
     ''' Makes predictions using a fit classifier. '''
@@ -72,39 +97,80 @@ def predict_labels(clf, features, target):
     print "Made predictions in {:.4f} seconds.".format(end - start)
     print "Classification Report: \n" + str(classification_report(target,y_pred))
 
-def pick_stocks():
+def predict_ret(reg, features, target):
+    ''' Makes predictions using a fit classifier. '''
+    
+    # Start the clock, make predictions, then stop the clock
+    start = time()
+    y_pred = reg.predict(features)
+    #pd.DataFrame(y_pred).to_csv('y_pred.csv')
+    end = time()
+    
+    # Print and return results
+    print "Made predictions in {:.4f} seconds.".format(end - start)
+    print "R2 Score: \n" + str(r2_score(target,y_pred))
 
-    dataset = pd.read_csv(join(FORMS_DATA_PATH,"picks_dataset.csv"))
+def count_zeroes(x):
+    count = 0
+    for i in x[0:12]:
+        if i == 0.0:
+            count = count + 1
+    return count
+
+
+def run_models(regression=False):
+
+    dataset = pd.read_csv("picker_dataset.csv")
     print "Data read successfully."
 
     #Cleanup data
-    #dataset.dropna(axis=0, inplace=True)
-    company_list = pd.Series(dataset['COMPANY'].copy(deep=True))
-    dataset = dataset.drop('COMPANY', axis=1)
+
+    dataset = dataset.drop(['RET1'], axis=1)
+    dataset = dataset.drop(['RET6'], axis=1)
+    dataset = dataset.drop(['RET12'], axis=1)
+    dataset = dataset[dataset.MONTH == '2016-01']
+    dataset = dataset.drop(['MONTH'], axis=1)
     dataset['MKTCAP'].replace('.*Small.', 'small', inplace=True, regex=True)
     dataset['MKTCAP'].replace('.*Mid.', 'mid', inplace=True, regex=True)
     dataset['MKTCAP'].replace('.*Large.', 'large', inplace=True, regex=True)
+    X_company = dataset['COMPANY']
+    dataset = dataset.drop(['COMPANY'], axis=1)
 
-    #Extract the feature and target columns
-    feature_cols = list(dataset.columns[:-1])
- 
+    #eliminates points with many starting zeroes
+    #dataset = dataset[dataset.apply(lambda x: count_zeroes(x) < 4,axis=1)]
+
+    #Extract the feature columns
+    feature_cols = list(dataset.columns)
+
+
     #Show the list of columns
     print "Feature columns:\n{}".format(feature_cols)
 
+    #Separate data into feature data and target data
     X_all = dataset[feature_cols]
+
     X_all = preprocess_features(X_all)
+    print "Processed feature columns ({} total features):\n{}".format(len(X_all.columns), list(X_all.columns))
 
-    print X_all.head()
-
-    clf = joblib.load('best_model1.pkl')
+    clf = joblib.load('bagging_nb_bear_final.pkl')
 
     y_pred = clf.predict(X_all)
 
-    rd = {'company': company_list, 'category': y_pred} 
-    rdf = pd.DataFrame(data=rd)
-    print rdf.category[0]
-    results = rdf[rdf.category.astype(np.str)==np.str('minus_ten')]
-    print results
+    #rd = {'company': company_list, 'category': y_pred} 
+    #rdf = pd.DataFrame(data=rd)
+    #print rdf.category[0]
+    #results = rdf[rdf.category.astype(np.str)==np.str('minus_ten')]
+    #print results
+
+
+    prob_above_clf = clf.predict_proba(X_all)[:,0]
+
+    sorted_prob = zip(X_company,clf.predict(X_all),prob_above_clf)
+    sorted_prob_df = pd.DataFrame(sorted_prob)
+    print sorted_prob_df.head()
+
+    sorted_prob_df.sort(2,ascending=False,inplace=True)
+    sorted_prob_df.to_csv('bear_picks.txt')
 
 if __name__ == "__main__":
-    pick_stocks()
+    run_models(regression=False)
--- trading.py
+++ trading.py
@@ -17,15 +17,18 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.metrics import brier_score_loss
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def preprocess_features(X):
-	output = pd.DataFrame(index = X.index)
-	for col, col_data in X.iteritems():
-		if col_data.dtype == object:
-			col_data = pd.get_dummies(col_data, prefix = col)
-		output = output.join(col_data)
-
-	return output
+    output = pd.DataFrame(index = X.index)
+    for col, col_data in X.iteritems():
+        if (col_data.dtype == object) and (col != 'MONTH') and (col != 'COMPANY'):
+            col_data = pd.get_dummies(col_data, prefix = col)
+        output = output.join(col_data)
+    return output
+        #if col_data.dtype == object:
 
 def performance_metric(y_true, y_pred):
 	return f1_score(y_true,y_pred,average='weighted')
@@ -38,10 +41,34 @@ def train_reg(reg, X_train, y_train, X_test, y_test):
     scoring_fnc = make_scorer(performance_metric)
     predict_ret(reg,X_test, y_test)
 
-def train_classifier(clf, X_train, y_train, X_test, y_test):
+def train_classifier(clf, X_train, y_train, X_test, y_test, X_company, X_month, y_test_raw):
     clf.fit(X_train, y_train) 
     scoring_fnc = make_scorer(performance_metric)
     predict_labels(clf,X_test, y_test)
+    prob_pos_clf = clf.predict_proba(X_test)[:,0]
+
+    #clf_isotonic = CalibratedClassifierCV(clf, cv=3, method='isotonic')
+    #clf_isotonic.fit(X_train, y_train)
+    #prob_pos_isotonic = clf_isotonic.predict_proba(X_test)[:, 1]
+    #predict_labels(clf_isotonic,X_test, y_test)
+    
+
+    sorted_prob = zip(X_company,X_month,clf.predict(X_test),y_test,y_test_raw,prob_pos_clf)
+    #sorted_prob = zip(X_company,X_month,clf_isotonic.predict(X_test),y_test,y_test_raw,prob_pos_isotonic)
+    sorted_prob_df = pd.DataFrame(sorted_prob)
+    print sorted_prob_df.head()
+
+    sorted_prob_df.sort_values(5,ascending=False,inplace=True)
+    #print sorted_prob_df.head(200)
+    print sorted_prob_df[sorted_prob_df[2]=='f1'].head(15)
+    print sorted_prob_df[sorted_prob_df[2]=='f2'].head(15)
+    print sorted_prob_df[sorted_prob_df[2]=='f3'].head(15)
+    #print sorted_prob_df[sorted_prob_df[2]=='f4'].head(15)
+    print("Brier scores: (the smaller the better)")
+    clf_score = brier_score_loss(y_test, prob_pos_clf, pos_label='f1')
+    print("No calibration: %1.3f" % clf_score)
+    #clf_score = brier_score_loss(y_test, prob_pos_isotonic, pos_label='f1')
+    #print("Isotonic calibration: %1.3f" % clf_score)
 
 def train_classifierGS(clf, X_train, y_train, params=None):
     cv_iters = 2
@@ -53,24 +80,21 @@ def train_classifierGS(clf, X_train, y_train, params=None):
     grid.fit(X_train, y_train)
     print "best_params_ for the optimal model are: {}.".format(str(grid.best_params_))
     return grid.best_params_, grid.best_estimator_
-    #print "cv_results.['params'][grid.best_params_] for the optimal model are: {}.".format(str(grid.cv_results_['params'][grid.best_index_]))      
 
-def train_classifierCV(clf, X_train, y_train, params=None, fit_params=None):
+def train_classifierCV(clf, X_train, y_train, fit_params=None):
     ''' Fits a classifier to the training data. '''
 
     cv_iters = 10
     cv_sets = ShuffleSplit(X_train.shape[0], n_iter=cv_iters, test_size=0.20, random_state=0)
     scoring_fnc = make_scorer(performance_metric)  
-    # Start the clock, train the classifier, then stop the clock
+
     start = time()
     scores_array = cross_val_score(clf, X_train, y=y_train, fit_params=fit_params, scoring=scoring_fnc, cv=cv_sets)
-    #grid.fit(X_train, y_train)
-    #clf.fit(X_train, y_train, sample_weight=weights)
     end = time()
     
     # Print the results
     print "Trained model in {:.4f} seconds".format(end - start)
-    print "\nCross-Validation scores for " + str(cv_iters) + " iterations:"
+    print "\nCross-Validation scores for " + str(cv_iters) + " iterations:\n"
     print scores_array
     
 def predict_labels(clf, features, target):
@@ -107,22 +131,20 @@ def count_zeroes(x):
     return count
 
 
-def run_models(regression=False):
+def run_models(regression=False, bear=True):
 
-    dataset = pd.read_csv("dataset_6_12_spy_alt_norm.csv")
+    dataset = pd.read_csv("dataset.csv")
     print "Data read successfully."
 
     #Cleanup data
-    dataset = dataset[dataset.RET1 != 'None']
-    dataset = dataset[dataset.RET1 != 'Fail']
-    dataset = dataset[dataset.RET6 != 'None']
-    dataset = dataset[dataset.RET6 != 'Fail']
+    #dataset = dataset[dataset.RET1 != 'None']
+    #dataset = dataset[dataset.RET1 != 'Fail']
+    #dataset = dataset[dataset.RET6 != 'None']
+    #dataset = dataset[dataset.RET6 != 'Fail']
     dataset = dataset[dataset.RET12 != 'None']
     dataset = dataset[dataset.RET12 != 'Fail']
     dataset.dropna(axis=0, inplace=True)
     #dataset = dataset.drop(['COMPANY','NBC1','NBC2','NBC3','NBC4','NBC5','NBC6','NBV1','NBV2','NBV3','NBV4','NBV5','NBV6'], axis=1)
-    dataset = dataset.drop(['COMPANY'], axis=1)
-    dataset = dataset.drop(['MONTH'], axis=1)
     dataset = dataset.drop(['RET6'], axis=1)
     dataset = dataset.drop(['RET1'], axis=1)
     dataset.rename(index=str, columns={'RET12': 'RET'}, inplace=True)
@@ -137,6 +159,7 @@ def run_models(regression=False):
     #dataset = dataset[dataset.apply(lambda x: count_zeroes(x) < 4,axis=1)]
 
     # TODO: Set the number of training points
+
     num_train = 80000
 
     #Print dataset statistics
@@ -149,6 +172,17 @@ def run_models(regression=False):
     first_third = np.percentile(ret_data,33)
     second_third = np.percentile(ret_data,66)
 
+
+    f1 = np.percentile(ret_data,33)
+    f2 = np.percentile(ret_data,66)
+    #f3 = np.percentile(ret_data,75)
+
+
+    print "F1: {:.4f}".format(f1)
+    print "F2: {:.4f}".format(f2)
+
+
+
     print "Min return: " + str(min_return) + '\n'
     print "Max return: " + str(max_return) + '\n'
     print "Mean return: " + str(mean_return) + '\n'
@@ -159,22 +193,39 @@ def run_models(regression=False):
 
     if regression == False:
         #Convert return data into categories
-        dataset.loc[(dataset.RET.astype(np.float64) > first_third) & (dataset.RET.astype(np.float64) < second_third), 'RETURN_CAT'] = np.str('middle')
-        dataset.loc[(dataset.RET.astype(np.float64) >= second_third), 'RETURN_CAT'] = np.str('above')
-        dataset.loc[(dataset.RET.astype(np.float64) <= first_third), 'RETURN_CAT'] = np.str('below')
+        #dataset.loc[(dataset.RET.astype(np.float64) > first_third) & (dataset.RET.astype(np.float64) < second_third), 'RETURN_CAT'] = np.str('middle')
+        #dataset.loc[(dataset.RET.astype(np.float64) >= second_third), 'RETURN_CAT'] = np.str('above')
+        #dataset.loc[(dataset.RET.astype(np.float64) <= first_third), 'RETURN_CAT'] = np.str('below')
+
+        #Convert return data into categories
+        y_raw = dataset['RET']
+
+        if bear:
+            dataset.loc[(dataset.RET.astype(np.float64) > f1) & (dataset.RET.astype(np.float64) < f2), 'RETURN_CAT'] = np.str('f2')
+            dataset.loc[(dataset.RET.astype(np.float64) <= f1), 'RETURN_CAT'] = np.str('f1')
+            dataset.loc[(dataset.RET.astype(np.float64) >= f2), 'RETURN_CAT'] = np.str('f3')
+        else:
+            dataset.loc[(dataset.RET.astype(np.float64) > f1) & (dataset.RET.astype(np.float64) < f2), 'RETURN_CAT'] = np.str('f2')
+            dataset.loc[(dataset.RET.astype(np.float64) <= f1), 'RETURN_CAT'] = np.str('f3')
+            dataset.loc[(dataset.RET.astype(np.float64) >= f2), 'RETURN_CAT'] = np.str('f1')
 
         #dataset.loc[(dataset.RETURN.astype(np.float64) > -0.07) & (dataset.RETURN.astype(np.float64) < 0.07), 'RETURN_CAT'] = np.str('within_seven')
         #dataset.loc[dataset.RETURN.astype(np.float64) >= 0.07, 'RETURN_CAT'] = np.str('plus_seven')
         #dataset.loc[dataset.RETURN.astype(np.float64) <= -0.07, 'RETURN_CAT'] = np.str('minus_seven')
 
-        print "\nNumber of stocks in middle: "
-        print dataset[dataset['RETURN_CAT']=='middle'].count()[0]
-        print "\nNumber of stocks above: "
-        print dataset[dataset['RETURN_CAT']=='above'].count()[0]
-        print "\nNumber of stocks below: "
-        print dataset[dataset['RETURN_CAT']=='below'].count()[0]
+        # print "\nNumber of stocks in middle: "
+        # print dataset[dataset['RETURN_CAT']=='middle'].count()[0]
+        # print "\nNumber of stocks above: "
+        # print dataset[dataset['RETURN_CAT']=='above'].count()[0]
+        # print "\nNumber of stocks below: "
+        # print dataset[dataset['RETURN_CAT']=='below'].count()[0]
 
-
+        print "\nNumber of stocks in f1: "
+        print dataset[dataset['RETURN_CAT']=='f1'].count()[0]
+        print "\nNumber of stocks f2: "
+        print dataset[dataset['RETURN_CAT']=='f2'].count()[0]
+        print "\nNumber of stocks f3: "
+        print dataset[dataset['RETURN_CAT']=='f3'].count()[0]
 
 
     #Extract the feature and target columns
@@ -190,41 +241,39 @@ def run_models(regression=False):
     y_all = dataset[target_col]
 
     X_all = preprocess_features(X_all)
-    print "Processed feature columns ({} total features):\n{}".format(len(X_all.columns), list(X_all.columns))
-
-
 
     # Set the number of testing points
     num_test = X_all.shape[0] - num_train
 
     # TODO: Shuffle and split the dataset into the number of training and testing points above
-    X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size = num_test, random_state = 1)
+    X_train, X_test, y_train, y_test, y_train_raw, y_test_raw = train_test_split(X_all, y_all, y_raw, test_size = num_test, random_state = 1)
+    X_company = X_test['COMPANY']
+    X_month = X_test['MONTH']
+    X_test = X_test.drop(['MONTH'], axis=1)
+    X_test = X_test.drop(['COMPANY'], axis=1)
+    X_train = X_train.drop(['MONTH'], axis=1)
+    X_train = X_train.drop(['COMPANY'], axis=1)
+    X_all = X_all.drop(['MONTH'], axis=1)
+    X_all = X_all.drop(['COMPANY'], axis=1)
+
+    print "Processed feature columns ({} total features):\n{}".format(len(X_train.columns), list(X_train.columns))
 
     # Show the results of the split
     #print "Training set has {} samples.".format(X_train.shape[0])
     #print "Testing set has {} samples.".format(X_test.shape[0])
-
-    # Generate weights for Naive Bayes samples
-    # y_weights = pd.Series(y_all.copy(deep=True))
-    # y_weights.loc[y_weights == 'plus_ten'] = 4
-    # y_weights.loc[y_weights == 'minus_ten'] = 4
-    # y_weights.loc[y_weights == 'plus_five'] = 1
-    # y_weights.loc[y_weights == 'minus_five'] = 1
-    # y_weights.loc[y_weights == 'plus_five_ten'] = 4
-    # y_weights.loc[y_weights == 'minus_five_ten'] = 4
-
-    params={'C': [100]}
+    
     #fit_params = {'sample_weight': y_weights}
     #fit_params = {'sample_weight': y_weights}
-    #clf_A = naive_bayes.GaussianNB()
+    #clf_B = naive_bayes.GaussianNB()
     #clf_A = svm.SVC(C=10, random_state = 2, cache_size=1000)
     #clf_A = KNeighborsClassifier(n_neighbors=3, metric='minkowski', p=3, weights='distance')
-    #clf_A = AdaBoostClassifier(random_state=2)
-    clf_A = BaggingClassifier(n_estimators=10, max_samples=0.5, max_features=0.5, random_state=2)
+    #clf_A = AdaBoostClassifier(clf_B,random_state=2)
+    clf_A = BaggingClassifier(clf_B, n_estimators=100, max_samples=0.3, max_features=0.3, random_state=2)
     #clf_A = RandomForestClassifier(random_state = 2)
-    #clf_gp = GaussianProcessClassifier(random_state = 2)
+    #clf_A = GaussianProcessClassifier(random_state = 2)
+    #clf_A = LinearDiscriminantAnalysis()
 
-    reg_A = svm.SVR(cache_size=1000)
+    #reg_A = svm.SVR(cache_size=1000)
 
     start = time()
     #best_param, best_estimator = train_classifierGS(clf_B, X_all, y_all, params)
@@ -233,11 +282,19 @@ def run_models(regression=False):
     if regression:
         train_reg(reg_A,X_train,y_train,X_test,y_test)
     else:
-        train_classifier(clf_A,X_train,y_train,X_test,y_test)
+        train_classifier(clf_A,X_train,y_train,X_test,y_test,X_company,X_month,y_test_raw)
 
+        #params={'n_estimators': [1,10,100,500,1000], 'max_samples': [0.3,0.5,1.0], 'max_features': [0.3,0.5,1.0]}
+        #train_classifierGS(clf_A, X_train, y_train, params=params)
+
+    clf_A.fit(X_all,y_all)
     end = time()
     print "Trained in {:.4f} seconds.".format(end - start)
-    joblib.dump(clf_A, 'ensemble.pkl')
+    if bear:
+        class_pref = 'bear'
+    else:
+        class_pref = 'bull'
+    joblib.dump(clf_A, 'bagging_nb_3' + class_pref + '.pkl')
 
 if __name__ == "__main__":
-    run_models(regression=False)
+    run_models(regression=False, bear=True)
